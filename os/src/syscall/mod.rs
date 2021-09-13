23ba5783302588d46ce679a11375ff39c4ec65d2
zhanghx0905 <731931282@qq.com>
2021-09-13 23:43:40 +0800

    fix type unmatching bug,  eliminate warning

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -32,7 +32,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 5]) -> isize {
         SYSCALL_EXEC => sys_exec(args[0] as *const u8),
         SYSCALL_WAITPID => sys_waitpid(args[0] as isize, args[1] as *mut i32),
         SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
-        SYSCALL_MMAP => sys_mmap(args[0], args[1], args[2], args[3], args[4] as isize),
+        SYSCALL_MMAP => sys_mmap(args[0], args[1], args[2], args[3], args[4]),
         SYSCALL_MUNMAP => sys_munmap(args[0], args[1]),
         SYSCALL_SET_PRIORITY => sys_set_priority(args[0] as isize),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
--- user/src/bin/test_shmem0.rs
+++ user/src/bin/test_shmem0.rs
@@ -18,7 +18,7 @@ pub fn main() -> i32 {
     let prot: usize = 3;
     let shmem_id = mmap(start0, len, prot, 1, -1);
     let addr0: *mut u8 = start0 as *mut u8;
-    let shmem_id = mmap(start1, len, prot, 1, shmem_id);
+    let _shmem_id = mmap(start1, len, prot, 1, shmem_id);
     let addr1: *mut u8 = start1 as *mut u8;
     unsafe {
         *addr0 = 97;
